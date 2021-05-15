b8ccfc8aee931dde323ee086eb6f1b61f7b55405
= <=>
2021-05-15 11:20:51 +0400

    udpate demo index.php file

--- index.php
+++ index.php
@@ -26,21 +26,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <style>
-        section {
-            padding: 60px;
-        }
+    <!-- UIkit CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.6.21/dist/css/uikit.min.css" />
 
-        .container {
-            max-width: 1100px;
-            margin: auto;
-            position: relative;
-        }
-    </style>
+    <!-- UIkit JS -->
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.6.21/dist/js/uikit.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.6.21/dist/js/uikit-icons.min.js"></script>
 
-    <script>
-        //const urlroot = "http://tinyeditor.localhost"
-    </script>
     <script src="tinyeditor/tinyeditor.js"></script>
 
     <title>APP</title>
@@ -48,31 +40,53 @@
 
 <body>
 
-    <section>
-        <div class="container">
+    <section class="uk-section">
+        <div class="uk-container">
+
+            <div class="editable">
+                <div class="editable-cage">
+                    <h1>Heading for table</h1>
+                </div>
+            </div>
+
             <div class="editable">
                 <div class="editable-cage">
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos perferendis aspernatur
                         necessitatibus quos possimus corporis praesentium dolorum laudantium alias consectetur excepturi
                         quidem, nam cum, tenetur, quas magni sapiente? Odit, adipisci.</p>
-                    <table style="border-collapse: collapse; width: 100%;" border="1">
-                        <tbody>
-                            <tr>
-                                <td style="width: 49.6364%;">one</td>
-                                <td style="width: 49.7273%;">two</td>
-                            </tr>
-                            <tr>
-                                <td style="width: 49.6364%;">three</td>
-                                <td style="width: 49.7273%;">four</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                </div>
+            </div>
+
+            <table class="uk-table uk-table-divideruk-table uk-table-striped editable">
+                <tbody class="editable-cage">
+                    <tr>
+                        <td style="width: 49.6364%;">one</td>
+                        <td style="width: 49.7273%;">two</td>
+                    </tr>
+                    <tr>
+                        <td style="width: 49.6364%;">three</td>
+                        <td style="width: 49.7273%;">four</td>
+                    </tr>
+                    <tr>
+                        <td style="width: 49.6364%;">three</td>
+                        <td style="width: 49.7273%;">four</td>
+                    </tr>
+                    <tr>
+                        <td style="width: 49.6364%;">three</td>
+                        <td style="width: 49.7273%;">four</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <div class="uk-container">
+
+            <div class="editable">
+                <div class="editable-cage">
                     <h1>This is something</h1>
                 </div>
             </div>
-        </div>
 
-        <div class="container">
             <div class="editable">
                 <div class="editable-cage">
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos perferendis aspernatur
@@ -110,7 +124,7 @@
             onDisabled: e => console.log('Disabled'),
             onClick: e => console.log('clicked'),
             onResponse: res => console.log(res),
-            onSave: data => console.log(data), // Optional
+            //onSave: data => console.log(data), // Optional
             tinymceConfig: {}
         });
     </script>
