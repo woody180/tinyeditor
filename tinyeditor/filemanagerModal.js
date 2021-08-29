0769881181b27c9faebe7632da1a385cb91eec84
= <=>
2021-08-29 10:18:29 +0400

    cleanng url when remove filemanager

--- tinyeditor/filemanagerModal.js
+++ tinyeditor/filemanagerModal.js
@@ -3,7 +3,7 @@ function filemanager(callback) {
     let fmHTML = `<div class="filemanager-container">
         <div class="pb-fm-header">
             <div class="pb-fm-heading">Page builder File manager</div>
-            <div onclick="(function(){ document.querySelector('.filemanager-container').remove(); })()" class="pb-fm-close uk-flex uk-flex-center uk-flex-middle" style="width: 30px; height: 30px;"><svg class="fg-editor-icon" style="width: 15px; height: 15px;" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 348.333 348.334" xml:space="preserve"> <g> <path d="M336.559,68.611L231.016,174.165l105.543,105.549c15.699,15.705,15.699,41.145,0,56.85 c-7.844,7.844-18.128,11.769-28.407,11.769c-10.296,0-20.581-3.919-28.419-11.769L174.167,231.003L68.609,336.563 c-7.843,7.844-18.128,11.769-28.416,11.769c-10.285,0-20.563-3.919-28.413-11.769c-15.699-15.698-15.699-41.139,0-56.85 l105.54-105.549L11.774,68.611c-15.699-15.699-15.699-41.145,0-56.844c15.696-15.687,41.127-15.687,56.829,0l105.563,105.554 L279.721,11.767c15.705-15.687,41.139-15.687,56.832,0C352.258,27.466,352.258,52.912,336.559,68.611z"></path> </g> </svg></div>
+            <div class="pb-fm-close uk-flex uk-flex-center uk-flex-middle" style="width: 30px; height: 30px;"><svg class="fg-editor-icon" style="width: 15px; height: 15px;" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 348.333 348.334" xml:space="preserve"> <g> <path d="M336.559,68.611L231.016,174.165l105.543,105.549c15.699,15.705,15.699,41.145,0,56.85 c-7.844,7.844-18.128,11.769-28.407,11.769c-10.296,0-20.581-3.919-28.419-11.769L174.167,231.003L68.609,336.563 c-7.843,7.844-18.128,11.769-28.416,11.769c-10.285,0-20.563-3.919-28.413-11.769c-15.699-15.698-15.699-41.139,0-56.85 l105.54-105.549L11.774,68.611c-15.699-15.699-15.699-41.145,0-56.844c15.696-15.687,41.127-15.687,56.829,0l105.563,105.554 L279.721,11.767c15.705-15.687,41.139-15.687,56.832,0C352.258,27.466,352.258,52.912,336.559,68.611z"></path> </g> </svg></div>
         </div>
         <div id="elfinder"></div>
     </div>`;
@@ -83,3 +83,11 @@ function filemanager(callback) {
         $('#elfinder').elfinder(options).elfinder('instance');
     }, 300);
 }
+
+
+
+// Close elFinder
+$('body').on('click', '.pb-fm-close', function(e) {
+    $('.filemanager-container').remove();
+    window.history.pushState({}, '', location.origin + location.pathname + location.search)
+})
